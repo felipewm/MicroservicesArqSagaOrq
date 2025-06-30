8fedce267b7720faaf3f1867b7603a06227fd22b
felipewm <fwmartirani@gmail.com>
2025-06-30 11:18:21 -0300

    correções de erros

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/kafka/KafkaConfig.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/kafka/KafkaConfig.java
@@ -1,6 +1,5 @@
 package br.com.microservices.orchestrated.orchestratorservice.config.kafka;
 
-import br.com.microservices.orchestrated.orchestratorservice.core.enums.ETopics;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -113,7 +112,7 @@ public class KafkaConfig {
         return buildTopic(PAYMENT_FAIL.getTopic());
     }
     @Bean
-    public NewTopic inventorySuccesssTopic() {
+    public NewTopic inventorySuccessTopic() {
         return buildTopic(INVENTORY_SUCCESS.getTopic());
     }
     @Bean
